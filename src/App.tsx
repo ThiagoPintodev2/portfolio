12f0bfc3ad1065b78624501b7fea29622cdd7e54
thiago.dev2 <thiago.dev2@gmail.com>
2026-01-18 19:07:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/header/Index'
+
+function App() {
+
+  return (
+    <div className={`bg-bgdark`}>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header/Index.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div className={`flex justify-around text-[2.5rem] font-pacifico h-[100vh]`}>
+      <div className={`text-ligthbrown`}>{"< Hello, I'm Thiago />"}</div>
+      <div className={`w-[50rem] h-[10rem] bg-bgdark2 rounded-2xl`}>ola</div>
+    </div>
+  );
+}
+
+export default Header;
