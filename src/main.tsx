03b03f3da142a5732919e940edd10b7682bd9e85
thiago.dev2 <thiago.dev2@gmail.com>
2026-02-24 17:00:19 -0300

    Fix 404 with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
-import React from "react";
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <HashRouter basename={import.meta.env.BASE_URL}>
+    <StrictMode>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </StrictMode>
+  </HashRouter>,
 );
