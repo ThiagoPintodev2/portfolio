70d2d3996de4056773a6adc763817b42aee92973
thiago.dev2 <thiago.dev2@gmail.com>
2026-02-24 17:21:08 -0300

    adjust int header

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter basename={import.meta.env.BASE_URL}>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <StrictMode>
       <App />
     </StrictMode>
-  </HashRouter>,
+  </BrowserRouter>,
 );
