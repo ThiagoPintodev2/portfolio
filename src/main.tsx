6a30ebf9917bf6ed3aaef533ee68d8a80038bd8f
thiago.dev2 <thiago.dev2@gmail.com>
2026-03-02 19:25:55 -0300

    added adjust in the Route

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
