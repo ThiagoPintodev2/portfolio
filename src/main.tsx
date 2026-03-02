6097ede340d8bee587569d4feddb715d6ec5f1d0
thiago.dev2 <thiago.dev2@gmail.com>
2026-03-02 19:23:32 -0300

    added adjust in the Route

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {  HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter basename={import.meta.env.BASE_URL}>
-    <StrictMode>
+  <StrictMode>
+    <HashRouter basename={import.meta.env.BASE_URL}>
       <App />
-    </StrictMode>
-  </HashRouter>
+    </HashRouter>
+  </StrictMode>,
 );
